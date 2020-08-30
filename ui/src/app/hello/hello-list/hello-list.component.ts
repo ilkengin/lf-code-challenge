2c4cebfd0b529928aca2225639cfd118aa8d934d
Ilkan Engin <ilkan.engin@gmail.com>
2020-08-30 13:02:45 +0300

    Now updating the list when item changes or new item is added

--- ui/src/app/hello/hello-list/hello-list.component.ts
+++ ui/src/app/hello/hello-list/hello-list.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HelloService } from '../hello.service';
 import { Greeting } from '../../core/models/greeting.model';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-hello-list',
   templateUrl: './hello-list.component.html',
   styleUrls: ['./hello-list.component.scss']
 })
-export class HelloListComponent implements OnInit {
+export class HelloListComponent implements OnInit, OnDestroy {
   greetings: Greeting[];
+
+  subscription: Subscription;
 
   constructor(
     private route: ActivatedRoute,
@@ -21,9 +24,23 @@ export class HelloListComponent implements OnInit {
     this.helloService.getAllMessages().subscribe(greetings => {
       this.greetings = greetings;
     });
+    this.subscription = this.helloService.changed().subscribe(this.greetingChanged.bind(this));
   }
 
   goToDetails(greeting: Greeting) {
     this.router.navigate([greeting.id], {relativeTo:this.route});
+  }
+
+  greetingChanged(greeting: Greeting) {
+    const index = this.greetings.findIndex(g => g.id === greeting.id)
+    if (index > -1) {
+      this.greetings[index] = greeting;
+    } else {
+      this.greetings.push(greeting);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
   }
 }
--- ui/src/app/hello/hello.service.ts
+++ ui/src/app/hello/hello.service.ts
@@ -1,11 +1,14 @@
 import { Injectable, OnInit } from '@angular/core';
 import { HelloAccessorService } from '../core/http-services/hello-accessor.service';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Greeting } from '../core/models/greeting.model';
 @Injectable({
     providedIn: 'root'
 })
 export class HelloService implements OnInit {
+
+    private changedSubject: Subject<Greeting> = new Subject(); 
 
     constructor(private helloAccessorService:HelloAccessorService) {}
 
@@ -26,6 +29,15 @@ export class HelloService implements OnInit {
     }
 
     public create(message: string) {
-        return this.helloAccessorService.create(message);
+        return this.helloAccessorService.create(message).pipe(
+            map(greeting => {
+                this.changedSubject.next(greeting);
+                return greeting;
+            })
+        );
+    }
+
+    public changed(): Observable<Greeting> {
+        return this.changedSubject.asObservable();
     }
 }
