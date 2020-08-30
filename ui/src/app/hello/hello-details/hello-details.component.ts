0e2c444dd91020f13f85f67faed8953eff9da35f
Ilkan Engin <ilkan.engin@gmail.com>
2020-08-30 13:37:28 +0300

    Fixed styling of details and Implemented update show mechanism

--- ui/src/app/hello/hello-details/hello-details.component.ts
+++ ui/src/app/hello/hello-details/hello-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HelloService } from '../hello.service';
 import { Greeting } from '../../core/models/greeting.model';
 import { ActivatedRoute } from '@angular/router';
+import { GeneralErrorStateMatcher } from '../general-error-state-matcher';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-hello-details',
@@ -10,14 +12,30 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HelloDetailsComponent implements OnInit {
   greeting: Greeting;
+  isEditing = false;
+  
+  matcher = new GeneralErrorStateMatcher();
+
+  greetingForm = new FormGroup({
+    message: new FormControl('', Validators.required),
+  });
 
   constructor(private route: ActivatedRoute, private helloService: HelloService) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     this.helloService.getMessage(id).subscribe(greeting => {
-      console.log('greeting: %o', greeting);
       this.greeting = greeting;
+      this.greetingForm.get('message').setValue(greeting.message);
     });
+  }
+
+  cancel() {
+    this.greetingForm.reset();
+    this.greetingForm.get('message').setValue(this.greeting.message);
+    this.isEditing = !this.isEditing;
+  }
+
+  save() {
   }
 }
