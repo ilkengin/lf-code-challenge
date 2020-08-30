cb552ea94373c67b3fbe70b3f967d641522e06e1
Ilkan Engin <ilkan.engin@gmail.com>
2020-08-30 12:22:04 +0300

    Fixed stylig issue

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatDialog } from '@angular/material/dialog';
+import { HelloCreateDialogComponent } from './hello/hello-create-dialog/hello-create-dialog.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  constructor() {}
+  constructor(public dialog: MatDialog) {
+
+  }
 
   ngOnInit(): void { }
+
+  openCreateDialog(): void {
+    const dialogRef = this.dialog.open(HelloCreateDialogComponent, {
+      width: '250px'
+    });
+  }
 }
